c767c68f3297c48ec718a8d2b780ddee82aa19d0
Angel Mondragon <amondrave54@gmail.com>
2023-04-16 16:34:52 -0500

    Estructura basica del proyecto

---
+++ src/main/java/com/celeus/controlinventario/ControlInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.celeus.controlinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celeus/controlinventario/ControlInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celeus.controlinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
